ed278e829c85757e8bf1ea1a5ba2522b8bf47586
xXuHaiyang <xuhaiyang@mail.ustc.edu.cn>
2023-04-13 21:50:19 +0800

    finish lab2 code

---
+++ Lab2/scripts/dist_train_dp.sh
@@ -0,0 +1,6 @@
+sh dist_train_m-s+dp-00+n-T+wd-005+r-T.sh
+sh dist_train_m-s+dp-01+n-T+wd-005+r-T.sh
+sh dist_train_m-s+dp-02+n-T+wd-005+r-T.sh
+sh dist_train_m-s+dp-03+n-T+wd-005+r-T.sh
+sh dist_train_m-s+dp-04+n-T+wd-005+r-T.sh
+sh dist_train_m-s+dp-05+n-T+wd-005+r-T.sh
