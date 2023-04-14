33af3529be58016b15f6cb90f9fb061049fd66e3
xXuHaiyang <xuhaiyang@mail.ustc.edu.cn>
2023-04-14 12:50:40 +0800

    remake lab2

---
+++ lab2/scripts/dist_train_e+n+r+wd.sh
@@ -0,0 +1,9 @@
+sh dist_train_m-b+dp-04+n-T+wd-005+r-T+e-240+i-192.sh
+
+sh dist_train_m-s+dp-01+n-F+wd-005+r-F.sh
+sh dist_train_m-s+dp-01+n-T+wd-005+r-F.sh
+sh dist_train_m-s+dp-01+n-F+wd-005+r-T.sh
+
+sh dist_train_m-s+dp-01+n-T+wd-01+r-T.sh
+sh dist_train_m-s+dp-01+n-T+wd-015+r-T.sh
+sh dist_train_m-s+dp-01+n-T+wd-02+r-T.sh
---
+++ lab2/scripts/dist_train_m-s+dp-01+n-T+wd-005+r-T+i-256.sh
@@ -0,0 +1,9 @@
+python -m torch.distributed.run --nproc_per_node=4 main.py \
+--input_size 256 --epochs 120 \
+--batch_size 32 --lr 4e-3 --update_freq 4 \
+--data_set TINY_IMNET \
+--data_path /mnt/nvme2/xuhaiyang/data/tiny-imagenet-200/ \
+--model convnext_small \
+--drop_path 0.1 --normalization true \
+--weight_decay 0.05 --residual true \
+--output_dir outputs/m-s+dp-01+n-T+wd-005+r-T+i-256
